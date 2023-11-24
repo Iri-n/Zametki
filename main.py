e1a950ec4bba9005ae0ec81b396c599d87fa8dc3
Irina Nunko <konyaevaik@gmail.com>
2023-11-24 21:07:58 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from tkinter import *
+from tkinter import messagebox
+from tkinter.filedialog import askopenfile, asksaveasfilename
+
+
+root = Tk()
+root.title("Заметки")
+root.geometry("400x400")
+
+menu_bar = Menu(root)
+file_menu = Menu(menu_bar)
+menu_bar.add_cascade(label="Файл", menu=file_menu)
+
+root.config(menu=menu_bar)
+root.mainloop()
+
